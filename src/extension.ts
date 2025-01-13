f814d31e86558e2966f29002cc3f34eff0d9cff9
CatBraaain <84499939+CatBraaain@users.noreply.github.com>
2025-01-13 18:37:20 +0900

    feat: use dispose for commands

--- src/extension.ts
+++ src/extension.ts
@@ -3,5 +3,7 @@ import vscode from "vscode";
 import { toggleFilesExclude } from "./commands";
 
 export function activate(context) {
-  vscode.commands.registerCommand("toggle-files-exclude.toggleFilesExclude", toggleFilesExclude);
+  context.subscriptions.push(
+    vscode.commands.registerCommand("toggle-files-exclude.toggleFilesExclude", toggleFilesExclude),
+  );
 }
